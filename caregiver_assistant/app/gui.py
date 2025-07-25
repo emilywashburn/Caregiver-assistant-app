743d425f8883600ff293a8e59ae26bb8f45a4cc6
emilywashburn <ewashbur1@ltu.edu>
2025-07-25 19:43:12 -0400

    Update project with final video and code

--- caregiver_assistant/app/gui.py
+++ caregiver_assistant/app/gui.py
@@ -9,12 +9,14 @@ title_font = ("Arial", 18, "bold")
 bg_color = "#f0f0f0"
 sidebar_color = "#548b54"
 
+#dictionary to hold patient tasks
 patient_tasks = {}
 
 #where the task data will be saved 
 DATA_FILE = "patients.json"
 COMPLETED_FILE="Completed_tasks.json"
 
+#side bar for navigation
 def add_sidebar(window, go_back=None):
     sidebar=tk.Frame(window, bg=sidebar_color, width=120)
     sidebar.pack(side="left", fill="y")
@@ -45,14 +47,17 @@ def add_sidebar(window, go_back=None):
         fg="white",
         command=window.quit        
     ).pack(padx=10, pady=10, fill="x")
-    
+   
+#clear completed tasks for the selected patient 
 def clear_completed_tasks(patient_name):
     if "_completed" in patient_tasks and patient_name in patient_tasks["_completed"]:
         patient_tasks["_completed"][patient_name] = []
         save_tasks_to_file()
         messagebox.showinfo("Completed Tasks Cleared", "All completed tasks have been cleared.")
-
-#loan existing tasks
+    else:
+        messagebox.showinfo("No Tasks found.", "No completed tasks found to clear.")
+        
+#load existing tasks
 def load_tasks():
     if os.path.exists(DATA_FILE):
         with open(DATA_FILE,"r") as f:
@@ -76,9 +81,11 @@ def save_completed_tasks():
     with open(COMPLETED_FILE, "w") as f:
         json.dump(completed_tasks, f)
         
+#load existing tasks when the program starts
 patient_tasks = load_tasks()
 completed_tasks = load_completed_tasks()  
 
+#convert lists to dicts 
 for patient, tasks in list(patient_tasks.items()):
     if isinstance(tasks, list):
         patient_tasks[patient] = {
@@ -94,7 +101,7 @@ def handle_patient_button():
         name = name.strip().lower()
         show_patient_tasks(name)
 
-#caregiver button gives option to view completed tasks
+#caregiver button 
 def handle_caregiver_button():
     name = simpledialog.askstring("Caregiver Access", "Enter patient name: ")
     if name:
@@ -161,6 +168,7 @@ def show_patient_tasks(patient_name):
     #list to hold checkbox variables
     checkbox_vars = []
     
+    #display the tasks in groups based on time of the day
     for time_period in ["morning", "afternoon", "evening"]:
         if tasks.get(time_period):
             tk.Label(
@@ -169,6 +177,7 @@ def show_patient_tasks(patient_name):
                 font=title_font,
                 bg="white"
             ).pack(pady=(10,0))
+            
     #display each task with a checkbox
             for task in tasks[time_period]: 
                 var = tk.BooleanVar()
@@ -235,7 +244,7 @@ def open_task_input_form(patient_name):
         "Household chores":["Do a load of Laundry", "Go Grocery Shopping", "Balance the check book", "Do the dishes", "Make the bed", "Sweep and mop", "Set the table", "Put away dishes", "Other"],
         "Meal time":["Make breakfast", "Make lunch", "Make dinner", "Have a snack", "Bake some cookies", "Other"],
         "Personal Care": ["Take a shower", "Take a bath", "Get dressed", "Brush your hair", "Brush your teeth", "Other"],
-        "Creative Activities":["Listen to music", "Look at family photos", "Play a card game", "Do a puzzle", "Read a book", "Watch a move","Other"],
+        "Creative Activities":["Listen to music", "Look at family photos", "Play a card game", "Do a puzzle", "Read a book", "Watch a movie","Other"],
         "Healthcare":["Take medication", "Other"],
         "Physical":["Go for a walk", "Plant flowers", "Water the plants", "Feed the birds", "Go to the park", "Have a picnic", "Sit on the porch with a drink", "Other"],
         "Miscellaneous":["Other"]
@@ -271,12 +280,15 @@ def open_task_input_form(patient_name):
     
     custom_task_var = tk.StringVar()
     
+    #updates the task checkboxes when a category is selected 
     def update_checkboxes(*args):
+        #clear existing checkboxes in the frame
         for widget in checkboxes_frame.winfo_children():
             widget.destroy()
             
         task_vars.clear()
         category = selected_category.get()
+        
         
         if category in categories:
             for task in categories[category]:
@@ -288,6 +300,7 @@ def open_task_input_form(patient_name):
         tk.Label(checkboxes_frame, text="Other:").pack(anchor="w", pady=(10,0))
         tk.Entry(checkboxes_frame, textvariable=custom_task_var).pack(anchor="w")
 
+#save selected tasks for the patients
     def save_selected_tasks():
         if patient_name not in patient_tasks:
             patient_tasks[patient_name] = {"morning": [], "afternoon": [], "evening": []}
@@ -329,7 +342,8 @@ def open_task_input_form(patient_name):
         text="Save Selected Tasks",
         command=save_selected_tasks
     ).pack(pady=10)
-        
+
+#create the caregivers main menu        
 def caregiver_menu(patient_name):
     #create the window 
     menu_window= tk.Toplevel()
@@ -370,7 +384,7 @@ def caregiver_menu(patient_name):
     width=20
     ).pack(pady=10)
 
-    
+#return to the home screen  
 def go_home(current_window):
     current_window.destroy()
     create_main_window()
